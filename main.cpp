3a510604b234df43c4693a21d1b853068146d0af
mitrich <mitrichdmitrich772@gmail.com>
2024-02-21 20:18:35 +0300

    initial

--- main.cpp
+++ main.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
 using namespace std;
 int main() {
-<<<<<<< HEAD
     cout << "Hello, World!" << endl;
-=======
     int integer;
     unsigned int un_integer;
     long long_integer;
@@ -11,6 +9,5 @@ int main() {
     short short_integer;
     string output = to_string(sizeof(integer)) + " " + to_string(sizeof(un_integer)) + " " + to_string(sizeof(long_integer)) + " "+ to_string(sizeof(integer_with_dot)) + " "+ to_string(sizeof(short_integer)) + " ";
     cout << output << endl;
->>>>>>> 75f03eb (initial)
     return 0;
 }
