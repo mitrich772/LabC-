8e71411fe100b9aab6a917bb2ca9959535733f67
mitrich772 <mitrichdmitrich772@gmail.com>
2024-02-25 21:36:10 +0300

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
@@ -22,6 +23,7 @@ int main() {
     unsigned short b9;                         //2
     unsigned char b8;                          //1 char	-128...127  unsigned char 0... 255
 
+
     cout << typeid(a1).name() << ":" << sizeof(a1) << endl;
     cout << typeid(a2).name() << ":" << sizeof(a2) << endl;
     cout << typeid(a3).name() << ":" << sizeof(a3) << endl;
@@ -41,5 +43,67 @@ int main() {
     cout << typeid(b9).name() << ":" << sizeof(b9) << endl;
     cout << typeid(b8).name() << ":" << sizeof(b8) << endl;
 
+    float all_f[6]{-1.1, -1.5, -1.9, 1.1, 1.5, 1.9};
+    cout << "Task 2";
+    cout << "float to int" << endl;
+    for (int i = 0; i < size(all_f); i++) {
+        int f = all_f[i];
+        int rf = (int) all_f[i];
+        cout << f << "|" << rf << endl; // отрезает после точки
+    }
+    double all_d[6]{-1.1, -1.5, -1.9, 1.1, 1.5, 1.9}; // -1,-1,-1,1,1,1
+    cout << "double to int" << endl;
+    for (int i = 0; i < size(all_d); i++) {
+        int f = all_d[i];
+        int rf = (int) all_d[i];
+        cout << f << "|" << rf << endl; // отрезает после точки
+    }
+    int y = true; // 1
+    int x = false; // 0
+    int yo = (bool) true; // 1
+    int xo = (bool) false; // 0
+    bool z = 5; // 1
+    bool p = 0; // 0
+    bool zo = (bool) 5; // 1
+    bool po = (bool) 0; // 0
+
+    cout << "true -> (int) true -> int = " << y << "|" << yo << endl;
+    cout << "false -> (int) false -> int = " << x << "|" << xo << endl;
+    cout << "5 -> (bool) 5 -> bool  = " << z << "|" << zo << endl;
+    cout << "0 -> (bool) 0 -> bool  = " << p << "|" << po << endl;
+
+
+
+    //3 задание
+    unsigned char ucс = 250; // 255
+    unsigned char cс = 250; // 255
+    short int k = pow(2, 15); // +- 2**15
+    unsigned short int uk = pow(2, 16); //2**16
+    long int l = pow(2,32); //2**32
+    ucс += 10;
+    cс += 10;
+    k += 1;
+    uk += 1;
+    l += 1;
+    cout << ucс << "|" << cс << endl;
+    cout << "short int :" << k << endl;
+    cout << "unsigned short int :" << uk << endl;
+    cout << "long int :" << l << endl;
+// 4 задание
+    unsigned  char uc;
+    char c;
+    uc = 150; // uc = 150
+    c = uc; // c = 150
+    cout << c << endl;
+    unsigned char uc1;
+    char c1;
+    c1 = -128; // c1 = -1 там символ пробела
+    uc1 = c1; // uc1 - макс тк -1 -> 255 -2 ->254 и тд
+    cout << "signed :" << (int)c1 << "unsigned :" << (int)uc1 << endl;
+    // Определяем максимальное значение unsigned long int
+    unsigned long int ling = -1;
+    cout <<"max of unsigned long int :" <<  ling << endl;
+
     return 0;
+
 }
